c88c344f98bba86476354dbcbd17a2ee7cc50368
rerorero <nato9598@hotmail.co.jp>
2014-09-20 14:00:59 +0900

    add github source link

--- link.sh
+++ link.sh
@@ -6,6 +6,7 @@ ln -sF ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/go/src/github.com ~/github
 mkdir ~/vimtmp
 mkdir ~/vimbackup
 
