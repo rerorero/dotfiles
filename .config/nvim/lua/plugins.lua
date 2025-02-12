e290ab3936aef93e7c6e88a3bfc3c1b6d07eb5de
rerorero <nato9598@hotmail.co.jp>
2025-02-12 21:26:25 +0900

    avante

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -47,10 +47,10 @@ require("lazy").setup({
     { "hashivim/vim-terraform" },
     -- lualine
     -- need to install Nerd Font https://chaika.hatenablog.com/entry/2019/06/09/090000
-    { "nvim-lualine/lualine.nvim", dependencies = { "kyazdani42/nvim-web-devicons"} },
+    { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons"} },
     -- filer
     -- need to install Nerd Font https://chaika.hatenablog.com/entry/2019/06/09/090000
-    { "kyazdani42/nvim-tree.lua", dependencies = { "kyazdani42/nvim-web-devicons" } },
+    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
     -- surround
     { "machakann/vim-sandwich" },
     -- editorconfig
@@ -69,7 +69,7 @@ require("lazy").setup({
     -- show all problems
     {
       "folke/trouble.nvim",
-      dependencies = { "kyazdani42/nvim-web-devicons" },
+      dependencies = { "nvim-tree/nvim-web-devicons" },
       config = function()
         require("trouble").setup {}
       end
@@ -87,90 +87,66 @@ require("lazy").setup({
     { "quangnguyen30192/cmp-nvim-ultisnips" },
     -- copilot
     { "github/copilot.vim" },
+    {
+      "yetone/avante.nvim",
+      event = "VeryLazy",
+      lazy = false,
+      version = "*", -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+      opts = {
+        provider = "openai", -- "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+        openai = {
+          -- endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o",
+          -- timeout = 30000, -- Timeout in milliseconds
+          -- temperature = 0,
+          -- max_tokens = 4096,
+        },
+      },
+      build = "make",
+      dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        --- The below dependencies are optional,
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        {
+          -- support for image pasting
+          "HakonHarnes/img-clip.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- recommended settings
+            default = {
+              embed_image_as_base64 = false,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
+              -- required for Windows users
+              use_absolute_path = true,
+            },
+          },
+        },
+        {
+          -- Make sure to set this up properly if you have lazy=true
+          'MeanderingProgrammer/render-markdown.nvim',
+          opts = {
+            file_types = { "markdown", "Avante" },
+          },
+          ft = { "markdown", "Avante" },
+        },
+      },
+    }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    frequency = 604800, -- 1 week
+  },
 })
-
--- local fn = vim.fn
--- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
--- if fn.empty(fn.glob(install_path)) > 0 then
---   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
--- end
---
--- vim.cmd([[
---   augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
---   augroup end
--- ]])
---
--- return require('packer').startup(function(use)
---   use 'wbthomason/packer.nvim' -- Packer can manage itself
---   use { "ellisonleao/gruvbox.nvim" }
---   use 'tomtom/tcomment_vim' -- comment multi lines (C--)
---   use 'kana/vim-submode' -- modify split size
---   use 'Yggdroot/indentLine' -- show vertical lines at each indentation level
---   use 'kassio/neoterm' -- resuable terminal
---   use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' } -- Install the fzf binary
---   use 'junegunn/fzf.vim' -- The lightning fast fzf fuzzy finder
---   use 'easymotion/vim-easymotion'
---   use 'prettier/vim-prettier' -- Prettier formatter
---   use 'jjo/vim-cue' -- cue highlight
---   use {
---     'nvim-telescope/telescope.nvim',
---     requires = { {'nvim-lua/plenary.nvim'} }
---   }
---   use 'hashivim/vim-terraform'
---   use {
---     'nvim-lualine/lualine.nvim',
---     -- need to install Nerd Font https://chaika.hatenablog.com/entry/2019/06/09/090000
---     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
---   }
---   use {
---       'kyazdani42/nvim-tree.lua', -- filer
---       -- need to install Nerd Font https://chaika.hatenablog.com/entry/2019/06/09/090000
---       requires = {'kyazdani42/nvim-web-devicons'},
---       tag = 'nightly' -- optional, updated every week. (see issue #1193)
---   }
---   use 'machakann/vim-sandwich' -- surround
---   use 'gpanders/editorconfig.nvim' -- editorconfig
---   -- lsp
---   use 'williamboman/mason.nvim' -- LSP installer
---   use 'williamboman/mason-lspconfig.nvim' -- LSP installer
---   use 'neovim/nvim-lspconfig' -- Easy LSP configuration
---   use 'fatih/vim-go' -- go plugin besides lsp (gopls)
---   use 'j-hui/fidget.nvim' -- show lsp progress
---   use 'simrat39/symbols-outline.nvim' -- show symboles
---   use {
---     'folke/trouble.nvim',
---     requires = "kyazdani42/nvim-web-devicons",
---     config = function()
---       require("trouble").setup {}
---     end
---   } -- show all problems
---   use 'vim-test/vim-test' -- test for Ruby
---   -- completion
---   use 'hrsh7th/cmp-nvim-lsp'
---   use 'hrsh7th/cmp-buffer'
---   use 'hrsh7th/cmp-path'
---   use 'hrsh7th/cmp-cmdline'
---   use 'hrsh7th/nvim-cmp'
---   -- snipets
---   use 'SirVer/ultisnips'
---   use 'quangnguyen30192/cmp-nvim-ultisnips'
---
---   -- copilot
---   use 'github/copilot.vim'
---
---   -- To install a new plugin, do PackerUpdate
---   
---   -- Automatically set up your configuration after cloning packer.nvim
---   -- Put this at the end after all plugins
---   if packer_bootstrap then
---     require('packer').sync()
---   end
--- end)
